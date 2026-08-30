7261290ba460bb0f142dc42a5c60124b761c27cf
Anthony S. <anthonyservellon4@gmail.com>
2026-08-29 20:20:05 -0600

    Se agrega calculadora básica y reglas de estándar de código

---
+++ CalculadoraAgil/CalculadoraAgil/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace CalculadoraAgil
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("--- Calculadora Ágil ---");
+            Console.Write("Ingresa el primer número: ");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Ingresa el segundo número: ");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine($"La suma es: {num1 + num2}");
+        }
+    }
+}
